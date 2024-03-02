200b3351f2a3087121fbabe948de02dd4d43907f
woosu <woosuk288@gmail.com>
2024-03-02 18:56:53 +0900

    ENH: notfound 화면 추가

---
+++ src/app/(default)/not-found.tsx
@@ -0,0 +1,31 @@
+import { HOME_PATH } from "@/constants/routes";
+import { HEADER_HEIGHT } from "@/constants/values";
+import { Button, Container, Typography, Stack } from "@mui/material";
+import NextLink from "next/link";
+
+export default function NotFound() {
+  console.log("notofund");
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: `calc(100vh - ${HEADER_HEIGHT})`,
+      }}
+    >
+      <Stack spacing={4} alignItems="center">
+        <Button variant="contained" color="primary" component={NextLink} href={HOME_PATH} replace>
+          &larr; Go Home
+        </Button>
+        <Typography variant="h4" align="center">
+          죄송합니다. <br /> 해당 페이지를 찾을 수 없습니다.
+        </Typography>
+        <Typography variant="h6" align="center">
+          최근 인기있는 도서들을 살펴보세요.
+        </Typography>
+      </Stack>
+    </Container>
+  );
+}
