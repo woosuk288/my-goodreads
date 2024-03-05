dc1df31f37021686089c56b07906ecdc5764d051
woosu <woosuk288@gmail.com>
2024-03-05 14:50:22 +0900

    ENH: LoadingProgress 컴포넌트에 circularProgressProps 추가

--- src/components/LoadingProgress.tsx
+++ src/components/LoadingProgress.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import CircularProgress from "@mui/material/CircularProgress";
+import CircularProgress, { CircularProgressProps } from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import { HEADER_HEIGHT } from "@/constants/values";
 
-const LoadingProgress = () => {
+interface Props {
+  circularProgressProps?: CircularProgressProps;
+}
+const LoadingProgress = ({ circularProgressProps }: Props) => {
   return (
     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: `calc(100vh - ${HEADER_HEIGHT})` }}>
-      <CircularProgress color="secondary" />
+      <CircularProgress color="secondary" {...circularProgressProps} />
     </Box>
   );
 };
