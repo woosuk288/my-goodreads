469034950c6ff5bc84b6da0f5ed1cf78843ff253
woosu <woosuk288@gmail.com>
2024-03-23 19:06:33 +0900

    BUG: SSR 경우 MUI 적용 안되는 문제 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,23 +3,28 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
+import LoadingProgress from "@/components/LoadingProgress";
 
 export const metadata = {
   title: "로그인",
   description: "카카오, 네이버, 이메일 로그인",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <React.Suspense fallback={<LoadingProgress circularProgressProps={{ color: "primary" }} />}>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              {props.children}
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </React.Suspense>
       </body>
     </html>
   );
