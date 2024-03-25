0ad8c449e7654c030af03550da3e3e67103c0ef2
woosu <woosuk288@gmail.com>
2024-03-25 22:54:14 +0900

    BUG: header boxshadow 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -220,7 +220,7 @@ const sxToolbar: SxProps = {
   // minHeight: "50px",
   height: "50px",
   justifyContent: "space-between",
-  boxShadow: "0 1px 2px rgba(0,0,0,0.15)",
+  // boxShadow: "0 1px 2px rgba(0,0,0,0.15)",
 
   "svg[aria-label='search']": {
     fontSize: "1.8rem",
