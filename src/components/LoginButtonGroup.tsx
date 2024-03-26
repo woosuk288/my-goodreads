e753945caa3e5cdae5720a9956161de439baefbc
woosu <woosuk288@gmail.com>
2024-03-26 16:03:00 +0900

    REF: 카카오 로그인시 무조건 계정정보 입력 화면 뜨도록 prompt=login 추가

--- src/components/LoginButtonGroup.tsx
+++ src/components/LoginButtonGroup.tsx
@@ -83,7 +83,7 @@ export default function LoginForm() {
   const handleSignInWithKakao = () => {
     const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
     const REDIRECT_URI = `${window.location.origin}/oauth/kakao`;
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`;
 
     navigation.push(KAKAO_AUTH_URL);
   };
