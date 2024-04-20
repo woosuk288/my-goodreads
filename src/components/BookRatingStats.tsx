29e16b9f693946014639065c85fe8a1f8d857d06
woosu <woosuk288@gmail.com>
2024-04-20 13:20:28 +0900

    BUG: 평점, 리뷰 0일 때 컴포넌트 안보이는 현상 수정

--- src/components/BookRatingStats.tsx
+++ src/components/BookRatingStats.tsx
@@ -23,7 +23,7 @@ const sxBookRatingStats: SxProps = {
 interface IBookRatingStats {
   ratingValue: number;
   userRatingCount: number;
-  userReviewCount?: number;
+  userReviewCount?: number; // 얘는 왜 중간에 undefined 뜨니?
   fontSize?: string;
   hasText?: boolean;
 }
@@ -31,26 +31,20 @@ interface IBookRatingStats {
 export default function BookRatingStats({ ratingValue, userRatingCount, userReviewCount, fontSize, hasText = true }: IBookRatingStats) {
   return (
     <Box sx={sxBookRatingStats} className="book_rating_Stats">
-      {ratingValue && (
-        <div className="rating_stats_col">
-          <Rating className="static_stars" name="read-only" value={1} max={1} readOnly size="small" />
-          <Typography className="static_stars_value">{ratingValue}</Typography>
-        </div>
-      )}
-      {userRatingCount && (
-        <div className="rating_stats_col">
-          <Typography fontSize={fontSize}>
-            {userRatingCount.toLocaleString()} {hasText && "ratings"}
-          </Typography>
-        </div>
-      )}
-      {userReviewCount && (
-        <div className="rating_stats_col">
-          <Typography>
-            {userReviewCount.toLocaleString()} {hasText && "reviews"}
-          </Typography>
-        </div>
-      )}
+      <div className="rating_stats_col">
+        <Rating className="static_stars" name="read-only" value={1} max={1} readOnly size="small" />
+        <Typography className="static_stars_value">{parseFloat((ratingValue / 2).toFixed(2))}</Typography>
+      </div>
+      <div className="rating_stats_col">
+        <Typography fontSize={fontSize}>
+          {userRatingCount.toLocaleString()} {hasText && "ratings"}
+        </Typography>
+      </div>
+      <div className="rating_stats_col">
+        <Typography>
+          {userReviewCount?.toLocaleString()} {hasText && "reviews"}
+        </Typography>
+      </div>
     </Box>
   );
 }
